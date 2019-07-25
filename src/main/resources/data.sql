a08188e3723f927bed494a61981993f8382c2f0f
EkoGroszek <jachimczak3125@gmail.com>
2019-07-25 13:12:59 +0200

    added H2 instead of My Sql Workbench

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO user(name,surname)VALUES ('Wiktor', 'Jachimczak');
+INSERT INTO user(name,surname)VALUES ('Robert', 'Witczyk');
+INSERT INTO user(name,surname)VALUES ('Jan', 'Maroszczyk');
+
+
+
+INSERT INTO account (name, balance,account_number, owner_id, currency)VALUES ('konto 1',200, '77116022020000100034364384', 1, 'PLN');
+INSERT INTO account (name, balance,account_number, owner_id, currency)VALUES ('konto 2',600, '77116022020000200034312384', 1, 'EUR');
+INSERT INTO account (name, balance,account_number, owner_id, currency)VALUES ('konto 3',500, '77116022020000003034364384', 2, 'PLN');
+INSERT INTO account (name, balance,account_number, owner_id, currency)VALUES ('konto 4',200, '77116022020000400034362134', 3, 'PLN');
