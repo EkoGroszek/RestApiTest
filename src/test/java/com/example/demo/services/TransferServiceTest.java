346072d4a06114f674e9191176c89446ddb0edf3
EkoGroszek <jachimczak3125@gmail.com>
2019-07-30 11:40:47 +0200

    added tests to TransferService

--- src/test/java/com/example/demo/services/TransferServiceTest.java
+++ src/test/java/com/example/demo/services/TransferServiceTest.java
@@ -1,7 +1,6 @@
 package com.example.demo.services;
 
 
-import com.example.demo.dao.AccountRepository;
 import com.example.demo.dao.TransferRepository;
 import com.example.demo.entities.Account;
 import com.example.demo.entities.Transfer;
@@ -12,29 +11,28 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
 
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
 
 @RunWith(SpringRunner.class)
 //@SpringBootTest
 public class TransferServiceTest {
 
-    @MockBean
     private TransferRepository transferRepository;
 
     @MockBean
     private TransferServiceImpl transferService;
 
     @MockBean
-    private AccountRepository accountRepository;
-
-    @MockBean
     private AccountServiceImpl accountService;
-
     @MockBean
     private JavaMailSender javaMailSender;
 
@@ -46,16 +44,51 @@ public class TransferServiceTest {
 
     private User user;
 
+    private Transfer transfer;
+
     @Before
     public void init() {
         user = new User();
         sendingAccount = new Account("76116022020000000034364384", new BigDecimal(200), user, "PLN", "Name");
         targetAccount = new Account("77116022020000000034364384", new BigDecimal(100), user, "PLN", "NameSecond");
-        accountRepository.save(sendingAccount);
-        accountRepository.save(targetAccount);
+        transfer = Transfer.builder()
+                .amount(BigDecimal.valueOf(10))
+                .sendingAccount(sendingAccount)
+                .targetAccount(targetAccount)
+                .ifSendEmail(false)
+                .build();
+
+        accountService = mock(AccountServiceImpl.class);
+        when(accountService.findByAccountNumber("76116022020000000034364384")).thenReturn(sendingAccount);
+        when(accountService.findByAccountNumber("77116022020000000034364384")).thenReturn(targetAccount);
+
+        when(accountService.save(sendingAccount)).thenReturn(sendingAccount);
+        when(accountService.save(targetAccount)).thenReturn(targetAccount);
+
+        transferRepository = mock(TransferRepository.class);
+        when(transferRepository.save(transfer)).thenReturn(transfer);
         transferService = new TransferServiceImpl(transferRepository, accountService, javaMailSender, targetAccountBalanceCalculator);
     }
 
+    @Test
+    public void transferAfterCreatingShouldHasStatusPending() {
+        //WHEN
+        Transfer result = transferService.createNewTransfer(transfer);
+        //THEN
+        Assert.assertThat(transfer.getStatus(), equalTo("PENDING"));
+    }
 
+    @Test
+    public void transferWithAddressEmailShouldCallSendEmailOnce() {
+        transfer.setIfSendEmail(true);
+        transferService.createNewTransfer(transfer);
+        verify(javaMailSender, times(1)).send((SimpleMailMessage) any());
+    }
+
+    @Test
+    public void transferWithoutAddressEmailShouldNotCallSendEmail() {
+        transferService.createNewTransfer(transfer);
+        verify(javaMailSender, times(0)).send((SimpleMailMessage) any());
+    }
 
 }
